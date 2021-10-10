a751ee2b494b9243778a9e048d72918d904e03f5
Vera <Gavrilovaveravera7@yandex.ru>
2021-10-10 21:06:08 +0300

    настроила коммит

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Post from './Post'
+import './styles/styles.css'
+
+const post = new Post('Webpack Post Title')
+
+console.log('Post to Stringg:', post.toString())
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        filename: '[name], [contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+                filename: './index.html',
+                template: 'index.html'
+            }
+        ),
+        new HTMLWebpackPlugin({
+                filename: './cards.html',
+                template: 'cards.html'
+            }
+        )
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    }
+}
